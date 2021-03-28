b007367088d39ae591818a95ff0f560e2ae04ba5
Gustavo Juneo <gustavojuneo@outlook.com>
2021-03-28 18:50:25 -0300

    add signIn with github and google and add to db

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query as q } from 'faunadb'
+
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
+
+import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
   providers: [
@@ -14,5 +18,26 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
       scope: 'read:user'
     })
-  ]
+  ],
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email, name, image } = user
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(q.Match(q.Index('user_by_email'), q.Casefold(email)))
+            ),
+            q.Create(q.Collection('users'), { data: { email, name, image } }),
+            q.Get(q.Match(q.Index('user_by_email'), q.Casefold(email)))
+          )
+        )
+
+        return true
+      } catch {
+        return false
+      }
+    }
+  }
 })
